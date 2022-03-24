3411473afeb325b40fc94b2e431fe5c328619893
Ayush Gupta <ayushgupta141001@gmail.com>
2022-03-25 00:35:24 +0530

    changes deleted

--- connect/connect.js
+++ connect/connect.js
@@ -6,7 +6,7 @@ require("dotenv").config()
 const app = express()
 
 const connect = async () => {
-    await mongoose.connect(process.env.DATABASE_URL)
+    await mongoose.connect(process.env.MONGO_URI)
 }
 
 module.exports = connect;
