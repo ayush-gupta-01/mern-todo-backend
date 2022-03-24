6bf259ad7712044a54f5e2b50fa769872faee933
Ayush Gupta <ayushgupta141001@gmail.com>
2022-03-25 00:30:37 +0530

    .

--- connect/connect.js
+++ connect/connect.js
@@ -6,7 +6,7 @@ require("dotenv").config()
 const app = express()
 
 const connect = async () => {
-    await mongoose.connect(process.env.MONGO_URL_LINK)
+    await mongoose.connect(process.env.DATABASE_URL)
 }
 
 module.exports = connect;
